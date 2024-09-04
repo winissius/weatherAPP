2db1aff1ea86ed2da9276279e060767acb3facd4
W. Machado <104407345+winissius@users.noreply.github.com>
2024-09-03 22:41:09 -0300

    HTML and CSS Initial
    Added HTML and CSS to homepage

---
+++ routes.py
@@ -0,0 +1,12 @@
+from main import app
+from flask import render_template
+
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/info")
+def info():
+    return "<p>infos</p>"
